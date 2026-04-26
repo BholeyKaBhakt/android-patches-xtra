ff2323975357035ff99b12edcf2616a6047ff0ae
BholeyKaBhakt <bholekabhakt@proton.me>
2026-04-26 20:12:23 +0530

    chore: minor cleanup

--- patches/src/main/kotlin/io/github/bholeykabhakt/patches/autosync/purchase/PurchaseAllItemsPatch.kt
+++ patches/src/main/kotlin/io/github/bholeykabhakt/patches/autosync/purchase/PurchaseAllItemsPatch.kt
@@ -1,10 +1,7 @@
 package io.github.bholeykabhakt.patches.autosync.purchase
 
-import app.morphe.patcher.Fingerprint
-import app.morphe.patcher.OpcodesFilter
 import app.morphe.patcher.patch.Compatibility
 import app.morphe.patcher.patch.bytecodePatch
-import com.android.tools.smali.dexlib2.Opcode
 import io.github.bholeykabhakt.patches.utils.logMatch
 import io.github.bholeykabhakt.patches.utils.returnEarly
 
